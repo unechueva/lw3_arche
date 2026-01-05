32c9ed919d243779ba15d20dd2736798574a5197
Kate Khlamova <katuhahlamova1481@gmail.com>
2026-01-05 14:57:23 +0300

    Implement countWords with space normalization

--- src/word_counter.cpp
+++ src/word_counter.cpp
@@ -1,1 +1,28 @@
-//
+#include "word_counter.h"
+#include <cctype>
+
+int countWords(const std::string& line) {
+    if (line.empty()) {
+        return 0;
+    }
+
+    int wordCount = 0;
+    bool inWord = false;
+
+    for (char c : line) {
+        if (std::isspace(static_cast<unsigned char>(c))) {
+            if (inWord) {
+                wordCount++;
+                inWord = false;
+            }
+        } else {
+            inWord = true;
+        }
+    }
+
+    if (inWord) {
+        wordCount++;
+    }
+
+    return wordCount;
+}
